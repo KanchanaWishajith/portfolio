1f184a9b18689e4970d2fa159d7983fb0830db22
Kanchana Samarasinghe <kanchana4592@gmail.com>
2025-03-15 19:15:11 +0530

    Modify navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,6 +5,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+      <Image src={assets.header_bg_color} alt='' className='w-full'/>
+    </div>
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
     flex items-center justify-between bg-white z-50'>
         <a href="#top">
@@ -12,7 +15,7 @@ const Navbar = () => {
             cursor-pointer mr-14'/>
         </a>
         <ul className='hidden md:flex items-center gap-6 lg:gap-8
-        rounded-full px-12 py-3'>
+        rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
             <li><a className='font-Ovo' href='#top'>Home</a></li>
             <li><a className='font-Ovo'href='#about'>About me</a></li>
             <li><a className='font-Ovo'href='#services'>Services</a></li>
