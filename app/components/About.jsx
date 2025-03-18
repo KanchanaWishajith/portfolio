09e5105f3d4f1b0d67a538c4696755d2f87f4cbf
Kanchana Samarasinghe <kanchana4592@gmail.com>
2025-03-18 14:04:58 +0530

    Services section update for darkmode

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,7 @@ import { assets, infoList, toolsData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-function About() {
+const About = ({isDarkMode}) => { 
   return (
     <div id='about' className='w-full px-[12%] py-10
     scroll-mt-20'>
@@ -26,17 +26,18 @@ function About() {
             {infoList.map(({icon, iconDark, title, description}, index)=>(
                     <li className="border-[0.5px] border-gray-400
                     rounded-xl p-6 cursor-pointer hover:bg-lightHover
-                    hover:-translate-y-1 duration-500 hover:shadow-black" key={index}
-                    >
-                        
-                        <Image src={icon} alt={iconDark} className='w-7 mt-3'/>
-                        <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
-                        <p className='text-gray-600 text-sm'>{description}</p>
+                    hover:-translate-y-1 duration-500 hover:shadow-black dark:border-white
+                    dark:hover:shadow-white dark:hover:bg-darkHover/50" 
+                    key={index}>
+                      <Image src={isDarkMode ? iconDark : icon} alt={title} className='w-7 mt-3'/>
+                        <h3 className='my-4 font-semibold text-gray-700
+                        dark:text-white'>{title}</h3>
+                        <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
                     </li>
                 ))}
 
         </ul>
-        <h4 className='my-6 text-gray-700 font-Ovo'>Tools I Use</h4>
+        <h4 className='my-6 text-gray-700 font-Ovo dark:text-white/80'>Tools I Use</h4>
 
         <ul className='flex items-center gap-4 sm:gap-5'>
           {toolsData.map((tool, index) => (
